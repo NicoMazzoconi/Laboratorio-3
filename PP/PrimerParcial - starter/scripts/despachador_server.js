40930c86f1cd40ee028953893eafe7ad1b363d61
NicoMazzoconi <nico.mazzoconi@gmail.com>
2019-06-18 16:13:50 -0300

    s

--- PP/PrimerParcial - starter/scripts/despachador_server.js
+++ PP/PrimerParcial - starter/scripts/despachador_server.js
@@ -32,7 +32,7 @@ function traerPersonas()
   xhr = new XMLHttpRequest();
   xhr.onreadystatechange = procesarPersonas;
   var cadena = 'http://localhost:3000/agregar';
-  var body = {'collection':'personas','objeto':persona}
+  var body = {'collection':'personas','objeto':persona};
   xhr.open('POST', cadena, true);
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send(JSON.stringify(body));     
@@ -40,18 +40,17 @@ function traerPersonas()
 
 function insertarPersona(persona)
 {
+  var body = {'collection':'personas','objeto':persona};
   $.ajax({
     url: "http://localhost:3000/agregar",
-    data,persona,
-    type:pots,
-    dataType: "application/json",
-
+    data: JSON.stringify(body),
+    type:"post",
+    contentType: 'application/json',
     beforeSend: function(){
         setSpinner(true);
     },
     success: function(respuesta){
         traerPersonas();
-        alert(respuesta);
         setSpinner(false);
     },
     error: function(xhr,status){
@@ -60,7 +59,7 @@ function insertarPersona(persona)
 });
 }
 
-function eliminarPersona(id)
+/*function eliminarPersona(id)
 {
   xhr = new XMLHttpRequest();
   xhr.onreadystatechange = procesarPersonas;
@@ -69,9 +68,30 @@ function eliminarPersona(id)
   xhr.open('POST', cadena, true);
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send(JSON.stringify(body));
+}*/
+
+function eliminarPersona(id)
+{
+  var body = {'collection':'personas','id':id};
+  $.ajax({
+    url: "http://localhost:3000/eliminar",
+    data: JSON.stringify(body),
+    type:"post",
+    contentType: 'application/json',
+    beforeSend: function(){
+        setSpinner(true);
+    },
+    success: function(respuesta){
+        traerPersonas();
+        setSpinner(false);
+    },
+    error: function(xhr,status){
+        alert("Error: " + status + " " + xhr.statusText);
+    }
+});
 }
 
-function modificarPersona(persona)
+/*function modificarPersona(persona)
 {
   xhr = new XMLHttpRequest();
   xhr.onreadystatechange = procesarPersonas;
@@ -80,6 +100,27 @@ function modificarPersona(persona)
   xhr.open('POST', cadena, true);
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send(JSON.stringify(body));
+}*/
+
+function modificarPersona(persona)
+{
+  var body = {'collection':'personas','objeto':persona};
+  $.ajax({
+    url: "http://localhost:3000/modificar",
+    data: JSON.stringify(body),
+    type:"post",
+    contentType: 'application/json',
+    beforeSend: function(){
+        setSpinner(true);
+    },
+    success: function(respuesta){
+        traerPersonas();
+        setSpinner(false);
+    },
+    error: function(xhr,status){
+        alert("Error: " + status + " " + xhr.statusText);
+    }
+});
 }
 
 function procesarPersonas()
